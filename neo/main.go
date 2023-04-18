9c236ae2b72fa052e375a6e4a9621539c6a3d542
jason <2631937454@qq.com>
2023-04-18 09:54:10 +0800

    :white_check_mark: 更新框架测试

--- neo/main.go
+++ neo/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"neo-web/neo/neo"
+	"fmt"
+	"github.com/borntodie-new/neo-web/neo/neo"
 	"net/http"
 )
 
 func main() {
 	engine := neo.New()
-	engine.GET("/user", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("user"))
+	engine.GET("/user", func(ctx *neo.Context) {
+		username := ctx.Query("username")
+		password := ctx.Query("password")
+		ctx.HTML(http.StatusOK, fmt.Sprintf("<h1>欢迎：%s</h1><br/><h1>你的密码是：%s<h1>", username, password))
 	})
-	engine.POST("/order", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("order"))
+	engine.POST("/login", func(ctx *neo.Context) {
+		username := ctx.PostForm("username")
+		password := ctx.PostForm("password")
+		ctx.JSON(http.StatusOK, struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}{
+			Username: username,
+			Password: password,
+		})
 	})
 	_ = engine.Run(":8080")
 }
