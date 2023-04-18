8f58c4061dab6ad78aee55d512460c306a3ae451
jason <2631937454@qq.com>
2023-04-18 14:43:57 +0800

    :construction: 完成trie树的定义

--- day03/neo/trie.go
+++ day03/neo/trie.go
@@ -1,4 +1,22 @@
 package neo
 
 type node struct {
+	pattern  string  // 叶子节点唯一标识 例如：/study/:lang
+	part     string  // 单个节点的唯一标识 例如：study、:lang
+	children []*node // 子节点 例如：[study, :lang]
+	isWild   bool    // 是否精确匹配，part含有 : 或 * 为true
+}
+
+// 查询子节点是否含有part节点
+// [/ , user, home]
+func (n *node) search(part string) *node {
+	if n.children == nil {
+		n.children = make([]*node, 0)
+	}
+	for _, child := range n.children {
+		if child.part == part {
+			return child
+		}
+	}
+	return nil
 }
