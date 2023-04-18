806a80193c5acb6715b0f74688a9a1aaa40de3af
jason <2631937454@qq.com>
2023-04-18 09:52:21 +0800

    :recycle: 引入上下文更新引擎逻辑

--- neo/neo/neo.go
+++ neo/neo/neo.go
@@ -1,21 +1,14 @@
 package neo
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandlerFunc 视图函数签名
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(ctx *Context)
 
 type Engine struct {
-	router map[string]HandlerFunc // 路由和视图作绑定【key是路由，value是视图函数】
-}
-
-// 内部核心API，仅共内部使用，用于注册路由
-func (e *Engine) addRouter(method string, pattern string, handlerFunc HandlerFunc) {
-	key := fmt.Sprintf("%s-%s", method, pattern)
-	e.router[key] = handlerFunc
+	*router
 }
 
 // GET 外部衍生API，提供给用户使用
@@ -34,18 +27,10 @@ func (e *Engine) PUT(pattern string, handlerFunc HandlerFunc) {
 
 // 对外对接用户，对内对接Web框架
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// 请求来了，需要匹配路由
-	url := r.URL.Path
-	method := r.Method
-	key := fmt.Sprintf("%s-%s", method, url)
-	handlerFunc, ok := e.router[key]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		_, _ = w.Write([]byte("NOT FOUND"))
-		return
-	}
-	// 执行命中的视图函数
-	handlerFunc(w, r)
+	// 构建Context上下文
+	ctx := NewContext(w, r)
+	// 转发请求到框架
+	e.handle(ctx)
 }
 
 // Run 手动启动服务，控制力强
@@ -54,5 +39,5 @@ func (e *Engine) Run(addr string) error {
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{newRouter()}
 }
